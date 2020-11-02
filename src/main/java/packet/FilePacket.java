9281b7c8ecf2996f6ec1e500c476b4fad57de933
Eduardo Lessa <duduuulessa@gmail.com>
2020-11-01 22:38:02 -0300

    Fix: FilePacket

--- src/main/java/packet/FilePacket.java
+++ src/main/java/packet/FilePacket.java
@@ -4,6 +4,31 @@ public class FilePacket extends BasePacket {
 
     private String fileName;
     private byte[] buff;
+    private int port;
+    private String ip;
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public FilePacket(String fileName, byte[] buff, int port, String ip) {
+        this.fileName = fileName;
+        this.buff = buff;
+        this.port = port;
+        this.ip = ip;
+    }
 
     public FilePacket(String fileName, byte[] buff) {
         this.fileName = fileName;
